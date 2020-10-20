c42e08f60da8a6f71fde4af5143f642d37fe85d3
Martin Kagume <martiniriga@gmail.com>
2020-10-20 17:39:05 +0300

    - Add home to sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,9 +4,9 @@
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
             <li class=" navigation-header"><span>General</span><i class="feather icon-minus" data-toggle="tooltip" data-placement="right" data-original-title="General"></i>
             </li>
-            <li class=" nav-item"><a href="index.html"><i class="feather icon-home"></i><span class="menu-title">Dashboard</span><span class="badge badge badge-primary badge-pill float-right mr-2">3</span></a>
+            <li class=" nav-item"><a href="{{ route('home') }}"><i class="feather icon-home"></i><span class="menu-title">Dashboard</span><span class="badge badge badge-primary badge-pill float-right mr-2">3</span></a>
                 <ul class="menu-content">
-                    <li><a class="menu-item" href="#">Dashboard</a>
+                    <li><a class="menu-item" href="{{ route('home') }}">Dashboard</a>
                     </li>
                 </ul>
             </li>
