32b8e5230dc2aa7acca319b5f0466796460deeee
Martin Kagume <martiniriga@gmail.com>
2020-10-22 01:14:42 +0300

    update migrations add timestamp

--- database/migrations/2020_10_03_175505_create_operationcosts_table.php
+++ database/migrations/2020_10_03_175505_create_operationcosts_table.php
@@ -16,6 +16,7 @@ class CreateOperationcostsTable extends Migration
         Schema::create('operationcosts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->timestamps();
         });
     }
 
