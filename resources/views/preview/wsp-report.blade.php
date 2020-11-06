fa0f7f36a4a571e6806cbb8927c899930c45df95
Martin Kagume <martiniriga@gmail.com>
2020-11-06 22:08:30 +0300

    update missing details on wsp report

--- resources/views/preview/wsp-report.blade.php
+++ resources/views/preview/wsp-report.blade.php
@@ -1,16 +1,16 @@
 <h4 class="text-center">WSP MONTHLY REPORTING</h4>
 <br>
-<p>Name of WSP: <b>{{$wsp_report->bcp->wsp->name}}</b></p>
-<p>Reporting period (month): <b>{{\DateTime::createFromFormat("!m",$wsp_report->month)->format("F")}} {{ $wsp_report->year }}</b></p>
-<p>Revenues collected this month (KES): <b>{{$wsp_report->revenue}}</b></p>
-<p>Total CLSG amount disbursed to date (KES): <b>{{$wsp_report->clsg_total}}</b></p>
+<p><b>Name of WSP:</b> {{$wsp_report->bcp->wsp->name}}</p>
+<p><b>Reporting period (month):</b> {{\DateTime::createFromFormat("!m",$wsp_report->month)->format("F")}} {{ $wsp_report->year }}</p>
+<p><b>Revenues collected this month (KES):</b> {{$wsp_report->revenue}}</p>
+<p><b>Total CLSG amount disbursed to date (KES):</b> {{$wsp_report->clsg_total}}</p>
 
-<h5>Status of resolution of issues (if any) raised in previous performance verification
-    reports</h5>
+<h5><b>Status of resolution of issues (if any) raised in previous performance verification
+    reports</b></h5>
 
-<p>Response:<b>{{$wsp_report->status_of_resolution ==1 ? 'Yes.' : 'No.'}}</b></p>
+<p><b>Response:</b>{{$wsp_report->status_of_resolution ==1 ? 'Yes.' : 'No.'}}</p>
 @if($wsp_report->status_of_resolution_comment)
-    <p>Comment:<b>{{$wsp_report->challenges_cooment}}</b></p>
+    <p><b>Comment:</b> {{$wsp_report->status_of_resolution_comment}}</p>
 @endif
 <br>
 <h5>O&M costs this month (KES):</h5>
@@ -18,22 +18,22 @@
     <thead>
     <tr>
         <th>Service</th>
-        <th>Description</th>
+        <th>Cost (KES)</th>
     </tr>
     </thead>
     <tbody>
-    @foreach(json_decode($wsp_report->status_of_covid_implementation) as $item)
+    @foreach(json_decode($wsp_report->operations_costs) as $item)
         <tr>
-            <td>{{$services->find($item->service)->name}}</td>
-            <td>{{$item->description}}</td>
+            <td>{{ $operationCosts->find($item->id)->name}}</td>
+            <td>{{ number_format($item->amount) }}</td>
         </tr>
     @endforeach
     </tbody>
 </table>
-<h5>Challenges (if any) encountered during the reporting period and mitigation measures</h5>
-<p>Response:<b>{{$wsp_report->challenges ==1 ? 'Yes.' : 'No.'}}</b></p>
-@if($wsp_report->challenges_cooment)
-    <p>Comment:<b>{{$wsp_report->challenges_cooment}}</b></p>
+<h5><b>Challenges (if any) encountered during the reporting period and mitigation measures</b></h5>
+<p><b>Response:</b>{{$wsp_report->challenges ==1 ? 'Yes.' : 'No.'}}</p>
+@if($wsp_report->challenges_comment)
+    <p><b>Comment:</b>{{$wsp_report->challenges_comment}}</p>
 @endif
 <h5>Expected activities for the next month (specifying any planned procurement or
     contracting)</h5>
@@ -41,15 +41,15 @@
     <table class="table table-bordered">
         <thead>
         <tr>
-            <th>Service</th>
-            <th>Amount</th>
+            <th>Activity</th>
+            <th>Description</th>
         </tr>
         </thead>
         <tbody>
-        @foreach(json_decode($wsp_report->operations_costs) as $item)
+        @foreach(json_decode($wsp_report->expected_activities) as $item)
             <tr>
-                <td>{{ $operationCosts->find($item->id)->name }}</td>
-                <td>{{ number_format($item->amount) }}</td>
+                <td>{{ $services->find($item->activity)->name }}</td>
+                <td>{{ $item->description }}</td>
             </tr>
         @endforeach
         </tbody>
