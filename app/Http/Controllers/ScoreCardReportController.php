39ed3b0edcbbc6c4b7896ad9f092cdf72f3e5ebb
Martin Kagume <martiniriga@gmail.com>
2020-11-09 09:49:24 +0300

    add review to score card

--- app/Http/Controllers/ScoreCardReportController.php
+++ app/Http/Controllers/ScoreCardReportController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use App\Http\Resources\PerformaceScoreResource;
 use App\Models\PerformanceScore;
 use App\Models\WspReporting;
+use App\Traits\ScoreCardAuthTrait;
+use App\Traits\SendMailNotification;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
 class ScoreCardReportController extends Controller
 {
+    use ScoreCardAuthTrait;
 
     public function index()
     {
@@ -82,5 +86,35 @@ class ScoreCardReportController extends Controller
         return response()->json($performance_score_card);
     }
 
+    public function review(PerformanceScore $report, Request $request)
+    {
+        $this->canAccessPerformanceScoreReport($report);
+        $report->status = $request->status;
+        $report->save();
+        $wsp = $report->bcp->wsp;
 
+        $route = route('performance-score-card.show', $report->id);
+        SendMailNotification::postReview($request->status, $wsp->id, $route, $wsp->name . ' Performance ScoreCard Review');
+
+        return response()->json([
+            'message' => 'Vulnerable Customer Report status changed to ' . $request->status,
+            'route' => $route
+        ]);
+    }
+
+    public function comment(PerformanceScore $report, CommentRequest $request)
+    {
+        $this->canAccessPerformanceScoreReport($report);
+
+        $report->comments()->create([
+            'description' => $request->description,
+            'user_id' => auth()->id()
+        ]);
+        $wsp = $report->bcp->wsp;
+
+        $route = route('performance-score-card.show', $report->id);
+        SendMailNotification::postComment($request->description, $report->status, $wsp->id, $route, $wsp->name . ' Performance ScoreCard Comment');
+
+        return response()->json(['message' => 'Comment posted successfully']);
+    }
 }
